f09efa3439015e13e3fb9a4ddfeffb5a15ba3175
Dimitar Pashev <dim.pashev@gmail.com>
2024-09-30 06:11:55 +0300

    added Django debug environ

--- src/cfehome/settings.py
+++ src/cfehome/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -7,10 +8,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-fg)#tp4kt77h(lci=3i$ssd2^vr(3sh5lau=x#1#(svfzf!9v&"
+SECRET_KEY = "django-insecure-ifik_s!mjunczxo@z6z3$lkklv)sqy1b)ckko$7e5%56wqvoga"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") or True
+print("DEBUG:", DEBUG, type(DEBUG))
 
 ALLOWED_HOSTS = [".railway.app"]  # https://saas.prod.railway.app
 if DEBUG:
@@ -60,7 +62,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "cfehome.wsgi.application"
-
+CSRF_TRUSTED_ORIGINS = ["https://*.railway.app"]
 
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
