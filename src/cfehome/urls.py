46a372ad2f7bc1492131de920bbe684496ad461f
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-06 16:14:41 +0300

    add login view

--- src/cfehome/urls.py
+++ src/cfehome/urls.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
 from django.urls import path
-
+from auth import views as auth_views
 from .views import home_view, about_view
 
 urlpatterns = [
     path("", home_view),  # index page -> root page
     path("about/", about_view),
+    path("login/", auth_views.login_view),
     path("hello-world/", home_view),
     path("hello-world.html", home_view),
     path("admin/", admin.site.urls),
