4d5ac02fbc1d7737e9e0dcfcc1b44501555486f8
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-07 07:24:40 +0300

    add register functionality

--- src/auth/views.py
+++ src/auth/views.py
@@ -1,5 +1,8 @@
 from django.contrib.auth import authenticate, login
 from django.shortcuts import render, redirect
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
 
 def login_view(request):
     if request.method == "POST":
@@ -13,5 +16,14 @@ def login_view(request):
                 return redirect("/")
     return render(request, "auth/login.html", {})
 
-# def register_view(request):
-#     return render(request, "auth/register.html", {})
+def register_view(request):
+    if request.method == "POST":
+        print(request.POST)
+        username = request.POST.get("username") or None
+        email = request.POST.get("email") or None
+        password = request.POST.get("password") or None  
+        try:         
+            User.objects.create_user(username, email=email, password=password)
+        except:
+            pass
+    return render(request, "auth/register.html", {})
