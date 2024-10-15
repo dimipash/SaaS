7a6f5a6fa6d157a5e7b7eab8c3ad17b0fdbabac4
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-15 13:36:10 +0300

    build subscription price model

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -42,13 +42,65 @@ class Subscription(models.Model):
     def save(self, *args, **kwargs):
         if not self.stripe_id:
             stripe_id = helpers.billing.create_product(
-                        name=self.name,
-                        metadata={
-                            "subscription_plan_id": self.id,
-                        },
-                        raw=False,
-                    )
-            self.stripe_id = stripe_id                    
+                name=self.name,
+                metadata={
+                    "subscription_plan_id": self.id,
+                },
+                raw=False,
+            )
+            self.stripe_id = stripe_id
+        super().save(*args, **kwargs)
+
+
+class SubscriptionPrice(models.Model):
+    """
+    Subscription Price = Stripe price
+    """
+
+    class IntervalChoices(models.TextChoices):
+        MONTHLY = "month", "Monthly"
+        YEARLY = "year", "Yearly"
+
+    subscription = models.ForeignKey(
+        Subscription, on_delete=models.SET_NULL, null=True
+    )
+    stripe_id = models.CharField(max_length=100, null=True, blank=True)
+    interval = models.CharField(
+        max_length=100, default=IntervalChoices.MONTHLY, choices=IntervalChoices.choices
+    )
+    price = models.DecimalField(max_digits=10, decimal_places=2, default=99.99)
+
+    @property
+    def stripe_currency(self):
+        return "usd"
+
+    @property
+    def stripe_price(self):
+        """
+        remove decimal places
+        """
+        return self.price * 100
+
+    @property
+    def product_stripe_id(self):
+        if not self.subscription:
+            return None
+        return self.subscription.stripe_id
+
+    def save(self, *args, **kwargs):
+        if (not self.stripe_id and self.product_stripe_id is not None):
+            stripe_id = helpers.billing.create_price(
+                currency=self.stripe_currency,
+                unit_amount=self.stripe_price,
+                interval=self.interval,                
+                product=self.product_stripe_id,
+                metadata={
+                    "subscription_plan_price_id": self.id,
+                },
+                raw=False
+            )
+            self.stripe_id = stripe_id
+
         super().save(*args, **kwargs)
 
 
