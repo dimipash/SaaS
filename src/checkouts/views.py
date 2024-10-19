4dfadc1190e7901c09b2515026e17a3d90778b4f
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-19 16:03:07 +0300

    add user subscription cancel view

--- src/checkouts/views.py
+++ src/checkouts/views.py
@@ -1,6 +1,7 @@
 import helpers.billing
 from django.shortcuts import render, redirect
 from django.urls import reverse
+from django.contrib import messages
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import get_user_model
 from django.conf import settings
@@ -103,6 +104,8 @@ def checkout_finalize_view(request):
         for k, v in updated_sub_options.items():
             setattr(_user_sub_obj, k, v)        
         _user_sub_obj.save()  
+        messages.success(request, "Success! Thank you for joining us.")
+        return redirect(_user_sub_obj.get_absolute_url())
 
     context = {}
     return render(request, "checkout/success.html", context)
