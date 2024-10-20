f4f6e0e33ee15894f1bfaa14d7ccc1efa8833b9d
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-20 17:40:32 +0300

    add custom command with arguments and custom model QuerySets and model managers

--- src/subscriptions/views.py
+++ src/subscriptions/views.py
@@ -4,18 +4,18 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from subscriptions.models import SubscriptionPrice, UserSubscription
+from subscriptions import utils as subs_utils
 
 @login_required
 def user_subscription_view(request):
     user_sub_obj, created = UserSubscription.objects.get_or_create(user=request.user)
     
-    if request.method == "POST":        
-        if user_sub_obj.stripe_id:
-            sub_data = helpers.billing.get_subscription(user_sub_obj.stripe_id, raw=False)
-            for k, v in sub_data.items():
-                setattr(user_sub_obj, k, v)
-            user_sub_obj.save()
+    if request.method == "POST":
+        finished = subs_utils.refresh_active_user_subscriptions(user_ids=[request.user.id])           
+        if finished:
             messages.success(request, "Your subscription has been refreshed")
+        else:
+            messages.warning(request, "Your subscription has not been refreshed")
         return redirect(user_sub_obj.get_absolute_url())      
     
     return render(request, "subscriptions/user_detail_view.html", {"subscription": user_sub_obj})
