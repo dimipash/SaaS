515b2e95ce0a354070b198ea013c2995abceb836
Dimitar Pashev <dim.pashev@gmail.com>
2024-10-22 13:26:51 +0300

    added STRIPE_TEST_OVERRIDE

--- src/helpers/billing.py
+++ src/helpers/billing.py
@@ -5,7 +5,9 @@ from . import date_utils
 DJANGO_DEBUG = config("DJANGO_DEBUG", default=False, cast=bool)
 STRIPE_SECRET_KEY = config("STRIPE_SECRET_KEY", default="", cast=str)
 
-if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG:
+STRIPE_TEST_OVERRIDE = config("STRIPE_TEST_OVERRIDE", default=False, cast=bool)
+
+if "sk_test" in STRIPE_SECRET_KEY and not DJANGO_DEBUG and not STRIPE_TEST_OVERRIDE:
     raise ValueError("Invalid stripe key for production")
 
 stripe.api_key = STRIPE_SECRET_KEY
